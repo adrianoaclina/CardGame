8d6e8db1b0acad12eef151b3ff9b86a60656ffe5
adriano.vaclina <adriano.vaclina@10.135.149.27>
2020-03-13 20:46:45 -0300

    Arrumando um bug de player 1 ou 2 atacando quando ele já está morto.

--- Card Game/Game.java
+++ Card Game/Game.java
@@ -10,7 +10,7 @@ public class Game {
 
 		Random sorteio = new Random();
 		int numero = sorteio.nextInt(10);
-		System.out.println(numero);
+//		System.out.println(numero);
 		if (numero % 2 == 1) {
 			System.out.println("O Player 1 vai realizar o primeiro ataque!!!");
 			while (player1.estaVivo() == true || player2.estaVivo() == true) {
@@ -18,19 +18,18 @@ public class Game {
 				player2.energia -= (danoRecebido = player2.defesa(player1.ataque()));
 				System.out.println("Player 1 causou: " + danoRecebido);
 				System.out.println("Player2 tem " + player2.energia);
-				player1.energia -= (danoRecebido = player1.defesa(player2.ataque()));
-				System.out.println("Player 2 causou: " + danoRecebido);
-				System.out.println("Player1 tem " + player1.energia);
-				if (player2.energia <= 0) {
-					System.out.print("Player 2 ganhou !!!");
-					break;
-				} else if(player2.energia <= 0) {
+				if(player2.energia <= 0) {
 					System.out.println("Player 1 ganhou !!!");
 					break;
 				}
-				
+				player1.energia -= (danoRecebido = player1.defesa(player2.ataque()));
+				System.out.println("Player 2 causou: " + danoRecebido);
+				System.out.println("Player1 tem " + player1.energia);
+				if (player1.energia <= 0) {
+					System.out.print("Player 2 ganhou !!!");
+					break;
+				}
 			}
-
 		} else {
 			System.out.println("O Player 2 vai realizar o primeiro ataque!!!");
 			while (player1.estaVivo() == true || player2.estaVivo() == true) {
@@ -38,13 +37,14 @@ public class Game {
 				player1.energia -= (danoRecebido = player1.defesa(player2.ataque()));
 				System.out.println("Player 2 causou: " + danoRecebido);
 				System.out.println("Player1 tem " + player1.energia);
-				player2.energia -= (danoRecebido = player2.defesa(player1.ataque()));
-				System.out.println("Player 1 causou: " + danoRecebido);
-				System.out.println("Player2 tem " + player2.energia);
 				if (player1.energia <= 0) {
 					System.out.print("Player 2 ganhou !!!");
 					break;
-				} else if(player2.energia <= 0) {
+				}
+				player2.energia -= (danoRecebido = player2.defesa(player1.ataque()));
+				System.out.println("Player 1 causou: " + danoRecebido);
+				System.out.println("Player2 tem " + player2.energia);
+				if(player2.energia <= 0) {
 					System.out.println("Player 1 ganhou !!!");
 					break;
 				}
